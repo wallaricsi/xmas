86c4aae414b4f1c80d9776782442556cb3db467b
wallaricsi <65926515+wallaricsi@users.noreply.github.com>
2020-10-18 17:27:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+document.querySelector("section h1").addEventListener("click", function() {
+    var c =0;
+    if (c ==0) {
+    document.getElementById("box").style.display = "block";
+    C = 1;
+} });
+
+document.querySelector(".close").addEventListener("click", function() {
+    if (C == 1) {
+    document.getElementById("box").style.display = "none";
+    c = 0;
+} });
